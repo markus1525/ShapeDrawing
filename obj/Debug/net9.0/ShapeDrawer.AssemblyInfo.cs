cda63bbd63f3ebe4c0e2f3075db7f2404ddcbe29
Min Thu Kyaw Khaung <204016955+markus1525@users.noreply.github.com>
2025-11-22 20:51:35 +0800

    Update project directory references and assembly information

--- obj/Debug/net9.0/ShapeDrawer.AssemblyInfo.cs
+++ obj/Debug/net9.0/ShapeDrawer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShapeDrawer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c375c91dfca45acc56f77a513a2387cf877c9e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f50b99e524025da3663033f2229091807b9e3829")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShapeDrawer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShapeDrawer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
