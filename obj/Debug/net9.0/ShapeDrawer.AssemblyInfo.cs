80c5d38be472866504fdc8011817ea0cf4726614
Min Thu Kyaw Khaung <204016955+markus1525@users.noreply.github.com>
2025-11-22 21:35:31 +0800

    Update .gitignore and assembly informational version for improved build management

--- obj/Debug/net9.0/ShapeDrawer.AssemblyInfo.cs
+++ obj/Debug/net9.0/ShapeDrawer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShapeDrawer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49a83d176074b366b82708af43f6c1b252ec6dde")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ae55655beb26abc79645901db97354895193acc")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShapeDrawer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShapeDrawer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
